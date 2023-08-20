fe8493bbef51e09c74eed97b87fa2ee269d16c3e
keisuzuki <kei.nishimura0131@gmail.com>
2023-08-20 09:56:11 +0900

    rails_session_login_task

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+    validates :name, presence: { message: "名前を入力してください" }, length: { maximum: 30 }
+    validates :email, presence: { message: "メールアドレスを入力してください" }, length: { maximum: 255 }, uniqueness: { message: "メールアドレスはすでに使用されています" }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    before_validation { email.downcase! }
+    has_secure_password
+    validates :password, presence: { message: "パスワードを入力してください" }, length: { minimum: 6, message: "パスワードは6文字以上で入力してください" }
+    validate :password_confirmation_matches
+
+    has_many :tasks
+
+  private
+
+  def password_confirmation_matches
+    if password.present? && password_confirmation.present? && password != password_confirmation
+      errors.add(:password_confirmation, "パスワード（確認）とパスワードの入力が一致しません")
+    end
+  end
+  
+end
