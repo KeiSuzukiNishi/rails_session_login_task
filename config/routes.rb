2f9d45a09f1a6a475bd624f38ca0921775ce94e7
keisuzuki <kei.nishimura0131@gmail.com>
2023-09-06 15:40:23 +0900

    rails_session_login_task_sep6_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy, :show]
   resources :users, only: [:new, :create, :show]
   resources :users do
     get 'edit', on: :member
   end
+  delete '/logout', to: 'sessions#destroy'
 end
